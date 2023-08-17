210ca987ba060e2322bb71f25c66113c9c66946e
Sabrina Yose Amelia <sabrina.amelia@student.matanauniversity.ac.id>
2023-08-17 20:21:19 +0700

    Update

--- src/list/PokemonList.ts
+++ src/list/PokemonList.ts
@@ -2,18 +2,29 @@ import { ref } from 'vue';
 
 export interface Pokemon {
   name: string;
-  height: number;
-  weight: number;
-  base_experience: number;
+  height?: number;
+  weight?: number;
+  base_experience?: number;
 }
 
 const pokemonLists = ref<Pokemon[]>([]);
 
-async function getPokemonList() {
+// specified offset value
+// async function getPokemonList() {
+//   const response = await fetch(
+//     'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20'
+//   );
+//   const data = await response.json();
+//   pokemonLists.value = data.results;
+//   return pokemonLists.value;
+// }
+
+async function getPokemonList(page: string) {
   const response = await fetch(
-    'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20'
+    'https://pokeapi.co/api/v2/pokemon/?offset='.concat(page, '&limit=20')
   );
   const data = await response.json();
+  // console.log(page);
   pokemonLists.value = data.results;
   return pokemonLists.value;
 }
@@ -23,8 +34,18 @@ async function getPokemonDetail(name: string) {
     'https://pokeapi.co/api/v2/pokemon/'.concat(name)
   );
   const data = await response.json();
-  //   pokemonLists.value = data.results;
+  // pokemonLists.value = data.results;
   return data;
 }
 
-export { getPokemonList, getPokemonDetail };
+async function getPokemonImg(name: string) {
+  const response = await fetch(
+    'https://pokeapi.co/api/v2/pokemon/'.concat(name)
+  );
+  const data = await response.json();
+  // pokemonLists.value = data.results;
+  console.log(data.sprites.front_default);
+  return data.sprites.front_default;
+}
+
+export { getPokemonList, getPokemonDetail, getPokemonImg };
